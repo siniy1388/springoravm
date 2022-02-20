e6697239ea6bb10711ba630d9bc5e39b3732584d
Oleg Sin <siniy1388@mail.ru>
2022-02-20 21:55:18 +0300

    20022022

---
+++ target/generated-sources/annotations/com/example/demo/entity/UserTable_.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-20T21:52:18")
+@StaticMetamodel(UserTable.class)
+public class UserTable_ { 
+
+    public static volatile SingularAttribute<UserTable, String> name;
+    public static volatile SingularAttribute<UserTable, Integer> id;
+    public static volatile SingularAttribute<UserTable, String> login;
+    public static volatile SingularAttribute<UserTable, String> email;
+
+}
---
+++ target/generated-sources/annotations/com/example/demo/entity/UserVm_.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-20T21:52:18")
+@StaticMetamodel(UserVm.class)
+public class UserVm_ { 
+
+    public static volatile SingularAttribute<UserVm, String> cdvmSystem;
+    public static volatile SingularAttribute<UserVm, Integer> cdvm;
+    public static volatile SingularAttribute<UserVm, String> name;
+    public static volatile SingularAttribute<UserVm, Long> id;
+    public static volatile SingularAttribute<UserVm, String> nameVm;
+    public static volatile SingularAttribute<UserVm, Integer> userid;
+
+}
