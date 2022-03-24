c7e4549e2e5e9a8e73f854bffb699ae977b60c78
siniy1388 <siniy1388@mail.ru>
2022-03-24 13:34:35 +0300

    Push from Idea

---
+++ target/generated-sources/annotations/com/example/demo/entity/Commands_.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-03-13T13:59:39")
+@StaticMetamodel(Commands.class)
+public class Commands_ { 
+
+    public static volatile SingularAttribute<Commands, String> groop;
+    public static volatile SingularAttribute<Commands, Long> id;
+    public static volatile SingularAttribute<Commands, String> nameR;
+    public static volatile SingularAttribute<Commands, String> command;
+
+}
