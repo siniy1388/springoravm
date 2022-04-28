930f76bb6dc8b34653ba50e1ebad8159ae573bc0
siniy_P <siniy_P@WIN-3SR53VD58RM>
2022-04-28 21:33:22 +0300

    Merge origin/winserv
    Conflicts:
    	src/main/java/com/example/demo/controller/OvmmController.java
    	src/main/java/com/example/demo/service/DefaultOvmmComndService.java
    	src/main/java/com/example/demo/service/OvmmComndConverter.java
    	src/main/java/com/example/demo/service/OvmmComndService.java
    	src/main/java/com/example/demo/sshservice/sshCommand.java

--- src/main/java/com/example/demo/controller/RoleForUserController.java
+++ src/main/java/com/example/demo/controller/RoleForUserController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,5 +31,29 @@ public class RoleForUserController {
     public List<RoleForUserDto> findAll() {
         log.info("Handling find all roles users request");
         return roleforuser.findAll();
+    }
+    
+     @GetMapping("/findByLogin")
+    public RoleForUserDto findByLogin(@RequestParam String login) {
+        log.info("Handling find all roles users request");
+        return roleforuser.findByLogin(login);
+    }
+    
+     @GetMapping("/findByRoleNm")
+    public RoleForUserDto findByRoleNm(@RequestParam String rolenm) {
+        log.info("Handling find all roles users request");
+        return roleforuser.findByRoleNm(rolenm);
+    }
+    
+     @GetMapping("/findByRoleid")
+    public RoleForUserDto findByRoleid(@RequestParam Integer roleid) {
+        log.info("Handling find all roles users request");
+        return roleforuser.findByRoleid(roleid);
+    }
+    
+     @GetMapping("/findByUserid")
+    public RoleForUserDto findByUserid(@RequestParam Integer userid) {
+        log.info("Handling find all roles users request");
+        return roleforuser.findByUserid(userid);
     }
 }
